16bcab41a2872f67345546edbd02165ba393f3fc
Alex Gascón <alexgascon.93@gmail.com>
2018-11-04 12:00:35 +0100

    Enabling import of todo list from a CSV file

--- lib/chapter_four/csv_importer.ex
+++ lib/chapter_four/csv_importer.ex
@@ -1,3 +1,30 @@
 defmodule ChapterFour.TodoList.CsvImporter do
+  alias ChapterFour.TodoList.TodoList
 
+  def import(file_path) do
+    file_path
+    |> read_lines
+    |> Stream.map(&parse_entry(&1))
+    |> Enum.to_list
+    |> TodoList.new
+  end
+
+  defp read_lines(file_path) do
+    File.read!(file_path)
+    |> String.split("\n")
+  end
+
+  def parse_entry(row) do
+    [date, title] = String.split(row, ",")
+
+    %{date: parse_date(date), title: title}
+  end
+
+  def parse_date(date) do
+    date
+    |> String.split("/")
+    |> Enum.map(&String.to_integer(&1))
+    |> List.to_tuple
+    |> Date.from_erl!
+  end
 end
